c55e8c23f8e99b59fc820eee05e265d8d929317e
LuckDev22 <103015314+LuckDev22@users.noreply.github.com>
2022-11-07 13:15:19 -0300

    feat/login

--- Scripts/api.js
+++ Scripts/api.js
@@ -1,1 +1,23 @@
 const baseUrl = 'https://m2-api-adot-pet.herokuapp.com/'
+
+export async function login(body) {
+    try {
+        const request = await fetch(baseUrl + "session/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json().then((response) => {
+            console.log(response)
+            window.location.assign("../Pages/myAdoptions/index.html")
+            localStorage.setItem("TokenLogin", response.token)
+        })
+        return response
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
