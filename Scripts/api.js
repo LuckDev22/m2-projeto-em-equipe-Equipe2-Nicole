4377f2162c279d0e2dd1ac91223f170a1e767e2e
Gustavo Lima <gcaastro1@gmail.com>
2022-11-08 11:19:49 -0300

    Create myProfile cards

--- Scripts/api.js
+++ Scripts/api.js
@@ -1,118 +1,159 @@
-const baseUrl = 'https://m2-api-adot-pet.herokuapp.com/'
+const baseUrl = "https://m2-api-adot-pet.herokuapp.com/"
 import { toast } from "./toast.js"
 
 export async function login(body) {
-    try {
-        const request = await fetch(baseUrl + "session/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(body)
-        })
-        const response = await request.json().then((response) => {
-            console.log(response)
-            localStorage.setItem("TokenLogin", response.token)
-            setTimeout(() =>{
-            window.location.assign("../home/index.html")
-            },2500)
-        })
-        return response
-    } catch (err) {
-        console.log(err)
-    }
+  try {
+    const request = await fetch(baseUrl + "session/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+    const response = await request.json().then((response) => {
+      console.log(response)
+      localStorage.setItem("TokenLogin", response.token)
+      setTimeout(() => {
+        window.location.assign("../home/index.html")
+      }, 2500)
+    })
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function getAllPets() {
-    const user = localStorage.getItem('TokenLogin')
-    try {
-        const request = await fetch(baseUrl + "pets", {
-            method: "GET",
-            headers: {
-                'Authorization': "Bearer " + user,
-            },
-        })
-        const response = await request.json().then((response) => {
-            return response
-        })
-        return response
-    } catch (err) {
-        console.log(err)
-    }
+  const user = localStorage.getItem("TokenLogin")
+  try {
+    const request = await fetch(baseUrl + "pets", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + user,
+      },
+    })
+    const response = await request.json().then((response) => {
+      return response
+    })
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function deletePet(id) {
-    const user = localStorage.getItem('TokenLogin')
-    try {
-        const request = await fetch(baseUrl + "pets/" + id, {
-            method: "DELETE",
-            headers: {
-                'Authorization': "Bearer " + user,
-            },
-        })
-        const response = await request.json().then((response) => {
-            return response
-        })
-        toast("Pet removido com sucesso!", "Todas as informações foram deletadas do banco de dados")
-        return response
-    } catch (err) {
-        console.log(err)
-    }
+  const user = localStorage.getItem("TokenLogin")
+  try {
+    const request = await fetch(baseUrl + "pets/" + id, {
+      method: "DELETE",
+      headers: {
+        Authorization: "Bearer " + user,
+      },
+    })
+    const response = await request.json().then((response) => {
+      return response
+    })
+    toast(
+      "Pet removido com sucesso!",
+      "Todas as informações foram deletadas do banco de dados"
+    )
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function adotePet(body) {
-    const user = localStorage.getItem('TokenLogin')
-    try {
-        const request = await fetch(baseUrl + "adoptions", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': "Bearer " + user,
-            },
-            body: JSON.stringify(body)
-        })
-        const response = await request.json().then((response) => {
-            return response
-        })
-        toast("Pet adotado!", "Parabéns o Pet foi adotado!")
-        return response
-    } catch (err) {
-        console.log(err)
-    }
+  const user = localStorage.getItem("TokenLogin")
+  try {
+    const request = await fetch(baseUrl + "adoptions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + user,
+      },
+      body: JSON.stringify(body),
+    })
+    const response = await request.json().then((response) => {
+      return response
+    })
+    toast("Pet adotado!", "Parabéns o Pet foi adotado!")
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function register(body, btn) {
-    try {
-        const request = await fetch(`${baseUrl}users`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        })
-        if (request.ok) {
-            toast("Conta criada com sucesso!", "Redirecionando para a página de login")
-            setTimeout(() => {
-                btn.innerText = "Cadastrar"
-                window.location.assign("/pages/login/index.html")
-            }, 2700)
-        }
-        const response = await request.json()
-        console.log(response)
-        if (response.message == "Email already in use") {
-            toast("Erro!", "E-mail já cadastrado.")
-            setTimeout(() => {
-                btn.innerText = "Cadastrar"
-            }, 4000)
-        }
-        if (response.message == "please inform a valid image link" || response.message == "'avatar_url' is required") {
-            console.log("toast avatar")
-            toast("Erro!", "Favor informar uma imagem válida.")
-            setTimeout(() => {
-                btn.innerText = "Cadastrar"
-            }, 4000)
-        }
-    } catch (err) {
-        return err
+  try {
+    const request = await fetch(`${baseUrl}users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+    if (request.ok) {
+      toast(
+        "Conta criada com sucesso!",
+        "Redirecionando para a página de login"
+      )
+      setTimeout(() => {
+        btn.innerText = "Cadastrar"
+        window.location.assign("/pages/login/index.html")
+      }, 2700)
     }
+    const response = await request.json()
+    console.log(response)
+    if (response.message == "Email already in use") {
+      toast("Erro!", "E-mail já cadastrado.")
+      setTimeout(() => {
+        btn.innerText = "Cadastrar"
+      }, 4000)
+    }
+    if (
+      response.message == "please inform a valid image link" ||
+      response.message == "'avatar_url' is required"
+    ) {
+      console.log("toast avatar")
+      toast("Erro!", "Favor informar uma imagem válida.")
+      setTimeout(() => {
+        btn.innerText = "Cadastrar"
+      }, 4000)
+    }
+  } catch (err) {
+    return err
+  }
+}
+
+export async function getMyAdoptions() {
+  const user = localStorage.getItem("TokenLogin")
+  try {
+    const request = await fetch(baseUrl + "adoptions/myAdoptions", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + user,
+      },
+    })
+    const response = await request.json()
+    return response
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function getMyProfile() {
+  const user = localStorage.getItem("TokenLogin")
+  try {
+    const request = await fetch(baseUrl + "users/profile", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + user,
+      },
+    })
+    const response = await request.json()
+    return response
+  } catch (err) {
+    console.log(err)
+  }
 }
